196a8054e50590f75ddd4c9908d9065410941742
Rawan22Hossam <rawankhedr22@gmail.com>
2023-03-23 03:16:52 -0700

    0x10 Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "vaiadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Print nums followed by new line
+ * @separator: the string printed btw numbers
+ * @n: number of the integers passed to function
+ * @...: A var number of numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list nums;
+unsigned int index;
+
+va_start(nums, n);
+for (index = 0; index < n; index++)
+{
+printf("%d", va_arg(nums, int));
+if (index != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(nums);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "vaiadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Print strings
+ * @separator: the string printed btw strings
+ * @n: number of the integers passed to function
+ * Return: no return
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list varlist;
+unsigned int i;
+char *str;
+
+va_start(valist, n);
+
+for (i = 0; i < n; i++)
+{
+str = va_arg(valist, char *);
+if (str)
+printf("%s", str);
+else
+printf("(nil)");
+if (i < n - 1)
+if (separator)
+printf("%s", separator);
+}
+printf("\n");
+va_end(valist);
+}
