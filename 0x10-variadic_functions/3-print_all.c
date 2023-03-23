a521606bec7df452c070d2ece01e2318bb0fe17e
Rawan22Hossam <rawankhedr22@gmail.com>
2023-03-23 03:29:30 -0700

    0x10 variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,12 +9,12 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list valist;
+va_list varlist;
 unsigned int i = 0, j, c = 0;
 char *str;
 const cahr t_arg[] = "cifs";
 
-va_start(valist, format);
+va_start(varlist, format);
 while (format && format[i])
 {
 j = 0;
@@ -30,16 +30,16 @@ j++;
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+printf("%c", va_arg(varlist, int)), c = 1;
 break;
 case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
+printf("%d", va_arg(varlist, int)), c = 1;
 break;
 case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
+printf("%f", va_arg(varlist, double)), c = 1;
 break;
 case 's':
-str = va_arg(valist, char *), c = 1;
+str = va_arg(varlist, char *), c = 1;
 if (!str)
 {
 printf("(nil)");
@@ -50,5 +50,5 @@ break;
 }
 i++;
 }
-printf("\n"), va_end(valist);
+printf("\n"), va_end(varlist);
 }
