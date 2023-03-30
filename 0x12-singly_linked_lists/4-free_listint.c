c0c298adfe26e04253c519c93233148b4c3ed33e
Rawan22Hossam <rawankhedr22@gmail.com>
2023-03-30 13:58:52 -0700

    update

--- 0x12-singly_linked_lists/4-free_listint.c
+++ 0x12-singly_linked_lists/4-free_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
-  * free_listint - define function
-  * @head: describe argument
-  */
+ * free_listint - frees all the node in a list.
+ * @head: pointer to a list.
+ */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
+	if (!head)
 		return;
 	free_listint(head->next);
 	free(head);
